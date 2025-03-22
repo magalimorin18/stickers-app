40fd9335b7ced3c92487c8e3f5fb2f359b5c4d4f
Magali Morin <magali.morin1@hotmail.fr>
2025-03-22 19:59:53 -0300

    feat: improve iamge loader

--- src/components/ContractConnecter.tsx
+++ src/components/ContractConnecter.tsx
@@ -1,5 +1,6 @@
 import { ethers } from "ethers";
 import { ContractAbi } from "../abi/CidCounterAbi";
+import { useState } from "react";
 
 interface ContractConecterProps {
   signer: ethers.Signer | null;
@@ -12,60 +13,75 @@ const ContractConnecter: React.FC<ContractConecterProps> = ({
 }) => {
   const contractAddress = "0x6914403E7873ABAa193Deb1Bdd712C47c1A7bA04";
   const contract = new ethers.Contract(contractAddress, ContractAbi, signer);
+  const [count, setCount] = useState(null);
+  const [added, setAdded] = useState(false);
+  const [voted, setVoted] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const addCid = async (cid: string) => {
     try {
+      setIsLoading(true);
       const tx = await contract.addCid(cid);
       await tx.wait();
       console.log("CID added successfully");
-    } catch (err) {
+      setAdded(true);
+    } catch (err: any) {
       console.error("Add CID failed:", err);
+      if (err.message.includes("CID already exists")) {
+        alert("💁‍♀️ Sticker already exists ");
+        setAdded(true);
+      }
     }
+    setIsLoading(false);
   };
 
   const incrementCid = async (cid: string) => {
     try {
+      setIsLoading(true);
       const tx = await contract.incrementCid(cid);
       await tx.wait();
       console.log("CID counter incremented");
-    } catch (err) {
+      setVoted(true);
+    } catch (err: any) {
       console.error("Increment failed:", err);
+      if (err.message.includes("CID does not exist")) {
+        alert("💁‍♀️ You need to add your sticker in the Smart Contract");
+      }
     }
+    setIsLoading(false);
   };
 
   const getCidCount = async (cid: string) => {
     try {
+      setIsLoading(true);
       const count = await contract.getCidCount(cid);
       console.log(`CID count: ${count.toString()}`);
+      setCount(count);
     } catch (err) {
       console.error("Get count failed:", err);
       return null;
     }
+    setIsLoading(false);
   };
 
   const handleAddCid = () => {
-    console.log("😃Add CID");
     if (!cid) return;
     addCid(cid);
   };
 
   const handleGetCidCound = () => {
-    console.log("😄Getting CID count");
     if (!cid) return;
 
     getCidCount(cid);
   };
 
   const handleIncrementCid = () => {
-    console.log("😄Incrementing CID");
     if (!cid) return;
-
     incrementCid(cid);
   };
 
   return (
-    <div>
-      <p>I am a contract interacting with CID {cid}</p>
+    <div className="flex-col">
       <button
         onClick={() => handleAddCid()}
         className="bg-blue-600 text-white px-4 py-2 rounded m-4 "
@@ -73,19 +89,31 @@ const ContractConnecter: React.FC<ContractConecterProps> = ({
         Add the sticker pack to the smart contract
       </button>
 
-      <button
-        onClick={() => handleGetCidCound()}
-        className="bg-blue-600 text-white px-4 py-2 rounded m-4"
-      >
-        Get number of votes
-      </button>
+      {isLoading && <p>⏳ Loading... </p>}
 
-      <button
-        onClick={() => handleIncrementCid()}
-        className="bg-blue-600 text-white px-4 py-2 rounded m-4"
-      >
-        Vote for this sticker pack
-      </button>
+      {added && (
+        <div>
+          <div className="flex-row">
+            <button
+              onClick={() => handleIncrementCid()}
+              className="bg-green-600 text-white px-4 py-2 rounded m-4"
+            >
+              Vote for this sticker pack
+            </button>
+            {voted && <p>🙌 Voted</p>}
+          </div>
+
+          <div className="flex-row">
+            <button
+              onClick={() => handleGetCidCound()}
+              className="bg-blue-600 text-white px-4 py-2 rounded m-4"
+            >
+              Get number of votes
+            </button>
+            {count && <p>Number of votes: {count}</p>}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -1,5 +1,5 @@
 // ImageUploader.tsx
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
 
 interface ImageUploaderProps {
   onImageUpload: (file: File) => void;
@@ -14,8 +14,6 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({
 }) => {
   const [, setImageFile] = useState<File | null>(null);
   const [imagePreview, setImagePreview] = useState<string | null>(null);
-  const [isDragging, setIsDragging] = useState(false);
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
   const handleFileChange = (fileList: FileList | null) => {
     if (fileList && fileList.length > 0) {
@@ -31,68 +29,34 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({
     setImagePreview(null);
   };
 
-  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    setIsDragging(true);
-  };
-
-  const handleDragLeave = () => {
-    setIsDragging(false);
-  };
-
-  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    setIsDragging(false);
-    if (e.dataTransfer?.files) {
-      handleFileChange(e.dataTransfer.files);
-    }
-  };
-
   return (
     <div className="main">
-      <div
-        className="dropzone-container"
-        onDragOver={handleDragOver}
-        onDragLeave={handleDragLeave}
-        onDrop={handleDrop}
-      >
-        <input
-          type="file"
-          ref={fileInputRef}
-          className="hidden-input"
-          accept=".jpg"
-          onChange={(e) => handleFileChange(e.target.files)}
-        />
+      <input
+        type="file"
+        className="hidden-input"
+        accept=".jpg"
+        onChange={(e) => handleFileChange(e.target.files)}
+      />
 
-        <label
-          onClick={() => fileInputRef.current?.click()}
-          className="file-label"
-        >
-          {isDragging
-            ? "Release to drop image here."
-            : "Drop image here or click here to upload."}
-        </label>
-
-        {imagePreview && (
-          <div className="preview-container mt-4">
-            <img
-              src={imagePreview}
-              alt="Uploaded Preview"
-              style={{
-                width: `${fixedWidth}px`,
-                height: `${fixedHeight}px`,
-                objectFit: "cover",
-              }}
-              className="rounded shadow"
-            />
-            <div>
-              <button className="mt-2 text-red-500" onClick={removeImage}>
-                Remove Image
-              </button>
-            </div>
+      {imagePreview && (
+        <div className="preview-container mt-4">
+          <img
+            src={imagePreview}
+            alt="Uploaded Preview"
+            style={{
+              width: `${fixedWidth}px`,
+              height: `${fixedHeight}px`,
+              objectFit: "cover",
+            }}
+            className="rounded shadow"
+          />
+          <div>
+            <button className="mt-2 text-red-500" onClick={removeImage}>
+              Remove Image
+            </button>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
